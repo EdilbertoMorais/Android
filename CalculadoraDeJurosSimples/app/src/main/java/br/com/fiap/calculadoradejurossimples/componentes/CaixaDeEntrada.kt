9cf2a820a0fc632da6adc67bcd7e843db324d34d
Edilberto Morais <edilbertocmorais@gmail.com>
2025-02-27 19:19:30 -0300

    implementando modularização e elevação de estado

---
+++ CalculadoraDeJurosSimples/app/src/main/java/br/com/fiap/calculadoradejurossimples/componentes/CaixaDeEntrada.kt
@@ -0,0 +1,67 @@
+package br.com.fiap.calculadoradejurossimples.componentes
+
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextFieldDefaults
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.tooling.preview.Preview
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun CaixaDeEntrada(
+    label: String,
+    labelColor: Color,
+    placeHolder: String,
+    placeHolderColor: Color,
+    value: String,
+    keyboardType: KeyboardType,
+    modifier: Modifier,
+    atualizarValor: (String) -> Unit,
+    singleLine: Boolean,
+    focusedBorderColor: Color = Color.Gray,
+    unfocusedBorderColor: Color = Color.LightGray,
+    textStyle: TextStyle
+) {
+    OutlinedTextField(
+        value = value,
+        onValueChange = atualizarValor,
+        label = { Text(text = label, style = TextStyle(color = labelColor)) },
+        placeholder = { Text(text = placeHolder, style = TextStyle(color = placeHolderColor)) },
+        modifier = modifier,
+        colors = TextFieldDefaults.outlinedTextFieldColors(
+            focusedBorderColor = focusedBorderColor,
+            unfocusedBorderColor = unfocusedBorderColor,
+            cursorColor = labelColor,
+            focusedLabelColor = labelColor,
+            unfocusedLabelColor = placeHolderColor
+        ),
+        textStyle = TextStyle(),
+        keyboardOptions = KeyboardOptions(keyboardType = keyboardType),
+        singleLine = singleLine
+    )
+}
+
+@Preview
+@Composable
+fun CaixaDeEntradaPreview(showSystemUi: Boolean = true) {
+    CaixaDeEntrada(
+        "Valor Investimento",
+        Color.White,
+        "Qual valor para investir",
+        Color.Yellow,
+        "teste",
+        KeyboardType.Text,
+        Modifier,
+        {},
+        true,
+        focusedBorderColor = Color.Yellow,
+        unfocusedBorderColor = Color.Magenta,
+        TextStyle(color = Color.White)
+    )
+}
---
+++ CalculadoraDeJurosSimples/app/src/main/java/br/com/fiap/calculadoradejurossimples/componentes/CardResultado.kt
@@ -0,0 +1,91 @@
+package br.com.fiap.calculadoradejurossimples.componentes
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import br.com.fiap.calculadoradejurossimples.JurosScreen
+
+@Composable
+fun CardResultado(juros: Double, montante: Double) {
+    Card(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(horizontal = 5.dp),
+        colors = CardDefaults.cardColors(
+            containerColor = Color(0xFF4CAF50)
+        )
+    ) {
+        Column(
+            modifier = Modifier
+                .padding(16.dp)
+        ) {
+            Text(
+                text = "Resultado:",
+                fontSize = 18.sp,
+                fontWeight = FontWeight.Bold,
+                color = Color.White
+            )
+            Spacer(modifier = Modifier.height(10.dp))
+            Row(
+                modifier = Modifier.fillMaxWidth()
+            ) {
+                Text(
+                    text = "Juros = R$",
+                    modifier = Modifier.padding(end = 8.dp),
+                    fontSize = 16.sp,
+                    fontWeight = FontWeight.Bold,
+                )
+                Text(
+                    text = juros.toString(),
+                    modifier = Modifier.padding(end = 8.dp),
+                    fontSize = 16.sp,
+                    fontWeight = FontWeight.Bold,
+                    color = Color.White
+                )
+//                Text(
+//                    text = "%",
+//                    modifier = Modifier.padding(end = 8.dp),
+//                    fontSize = 18.sp,
+//                    fontWeight = FontWeight.Bold,
+//                )
+            }
+            Spacer(modifier = Modifier.height(8.dp))
+            Row(
+                modifier = Modifier.fillMaxWidth()
+            ) {
+                Text(
+                    text = "Montante = R$",
+                    modifier = Modifier.padding(end = 8.dp),
+                    fontSize = 16.sp,
+                    fontWeight = FontWeight.Bold
+                )
+                Text(
+                    text = montante.toString(),
+                    modifier = Modifier.padding(end = 8.dp),
+                    fontSize = 16.sp,
+                    fontWeight = FontWeight.Bold,
+                    color = Color.White
+                )
+            }
+        }
+    }
+}
+
+@Preview
+@Composable
+fun CardResultadoPreview(showSystemUi: Boolean = true) {
+    CardResultado(juros = 2.0, montante = 180.0)
+}
