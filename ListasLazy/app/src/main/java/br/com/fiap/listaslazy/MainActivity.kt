9837f690b75aa8e9ac5bd2387afa14703ce9082d
Edilberto Morais <edilbertocmorais@gmail.com>
2025-03-06 15:04:32 -0300

    Aprendendo a lidar com listas com o LazyColumn

---
+++ ListasLazy/app/src/main/java/br/com/fiap/listaslazy/MainActivity.kt
@@ -0,0 +1,100 @@
+package br.com.fiap.listaslazy
+
+import android.os.Bundle
+import android.util.Log
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Search
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import br.com.fiap.listaslazy.repository.getAllGames
+import br.com.fiap.listaslazy.repository.getGamesByStudio
+import br.com.fiap.listaslazy.ui.theme.ListasLazyTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        Log.i("aaa", getGamesByStudio("Capcom").toString())
+        setContent {
+            ListasLazyTheme {
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    GamesScreen()
+                }
+            }
+        }
+    }
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun GamesScreen() {
+    Column(modifier = Modifier.padding(16.dp)) {
+        Text(
+            text = "Lista de Jogos Favoritos",
+            fontSize = 24.sp,
+            fontWeight = FontWeight.Bold,
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(top = 16.dp),
+            textAlign = TextAlign.Center
+        )
+        Spacer(modifier = Modifier.height(16.dp))
+        OutlinedTextField(
+            value = "",
+            onValueChange = {},
+            modifier = Modifier.fillMaxWidth(),
+            label = {
+                Text(text = "Nome do estúdio")
+            },
+            trailingIcon = {
+                IconButton(onClick = { /*TODO*/ }) {
+                    Icon(
+                        imageVector = Icons.Default.Search,
+                        contentDescription = ""
+                    )
+                }
+            }
+        )
+        Spacer(modifier = Modifier.height(16.dp))
+        LazyColumn() {
+            items(getAllGames().size) {
+                Text(text = getAllGames()[it].title)
+            }
+        }
+    }
+}
+
+
+
+@Preview(
+    showBackground = true,
+    showSystemUi = true,
+    uiMode = 50
+)
+@Composable
+fun GamesScreenPreview() {
+    GamesScreen()
+}
